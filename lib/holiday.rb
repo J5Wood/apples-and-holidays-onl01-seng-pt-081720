4f36302146b8f0045146132f58126a6db6511dba
Jeremy Wood <js.wood206@gmail.com>
2020-08-29 05:20:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -79,8 +79,9 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |current_season, holidays|
     puts current_season.to_s.capitalize + ":"
     holidays.each do |holiday, supplies|
-      puts "  " + holiday.to_s.capitalize + ": " + supplies.join(", ")
       binding.pry
+      puts "  " + holiday.to_s.capitalize + ": " + supplies.join(", ")
+      
     end
   end
 end
