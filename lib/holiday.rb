aae6a29d49e0a1e0e4e568668630f3aee25d4007
Jeremy Wood <js.wood206@gmail.com>
2020-08-29 04:23:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -27,9 +27,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, holiday|
     if season == :winter
-      holiday.each do |supplies|
-        supplies = supplies << supply
-        supplies.flatten
+      season[holiday] : supply
         binding.pry
       end
     end
