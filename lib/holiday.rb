75bb42f302c1592ec4e723a8885177ba12b9c4d7
Jeremy Wood <js.wood206@gmail.com>
2020-08-29 05:09:55 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,10 +77,9 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |current_season, holidays|
-    current_season.to_s.capitalize
-    binding.pry
     holidays.each do |holiday, supplies|
-      puts holidays.to_s.capitalize + ":"
+      current_season.to_s.capitalize
+      holidays.to_s.capitalize + ":"
       supplies.to_s.capitalize
       binding.pry
     end
