154090662872946f869f9e064d5b1c7251f73aac
Jeremy Wood <js.wood206@gmail.com>
2020-08-29 05:31:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,5 +89,6 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.each do |current_season, holidays|
+    binding.pry
 end
